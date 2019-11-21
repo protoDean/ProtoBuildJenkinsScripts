6a1a2da4a196d1e23882ab31385bc97820ed6a57
dean@protostargames.com <dean@protostargames.com>
2019-11-21 10:20:36 +1000

    Dean added dailybuild and test

---
+++ Pipelines/dailyBuild.groovy
@@ -0,0 +1,88 @@
+node{
+
+        def buildProfile = "iosRelease"
+        def projectFolder = env.projectFolder
+        def sourceBranch = "default"
+        
+        //Grab the build num from the release build, and make the debug build the same. So we can swap between them.
+        def finalBuildNumber
+        def outputFolder
+        
+        
+        buildProfile = "iosRelease"
+        outputFolder =  "${projectFolder}Daily"
+        //final DAILY_BUILD_TEMP = "DailyBuildTemp" + currentBuild.getStartTimeInMillis() 
+        //def dailyBuildFolder = DAILY_BUILD_TEMP
+        //ef buildPath = "../DailyBuilds/"+ DAILY_BUILD_TEMP
+		def buildPath = "../DailyBuilds/DailyBuild" + currentBuild.number
+        
+        def commonParams = [
+            [$class: 'StringParameterValue', name: 'projectFolder', value: projectFolder],
+            [$class: 'StringParameterValue', name: 'outputFolder', value: outputFolder] ,
+            [$class: 'StringParameterValue', name: 'sourceBranch', value: sourceBranch],
+            [$class: 'StringParameterValue', name: 'unityVersion', value: unityVersion],
+            [$class: 'StringParameterValue', name: 'buildPath', value: buildPath]
+            ]
+        
+        stage("ReleaseBuildIos" + projectFolder) {
+    
+            def finalBuildResult = build job: 'IosReleaseBuild', parameters: commonParams + [
+                [$class: 'StringParameterValue', name: 'buildProfile', value: buildProfile] 
+                ], propagate: false, wait: true
+                
+                
+            finalBuildNumber = "" + finalBuildResult.number
+            
+            //def finalBuildCompletePath = finalBuildResult.getBuildVariables().WORKSPACE + "/" + dailyBuildFolder
+            //dailyBuildFolder =  "DailyBuild" + finalBuildNumber
+            
+            //sh "echo ${dailyBuildFolder}"
+
+        }
+        
+        buildProfile = "iosDebug"
+            
+        stage("DebugBuildIos" + projectFolder) {
+      
+            build job: 'IosDevBuild', parameters: commonParams + [
+                [$class: 'StringParameterValue', name: 'buildProfile', value: buildProfile],
+                [$class: 'StringParameterValue', name: 'buildNumOverride', value: finalBuildNumber]
+                ], propagate: false, wait: true
+        }
+        
+        //rename folder
+        //sh "mv -v " + DAILY_BUILD_TEMP + " " + dailyBuildFolder
+        //Upload Release build if neccessary.
+        
+         buildProfile = "googleDebugApk"
+         stage("DevBuildAndroid" + projectFolder) {
+    
+            def finalBuildResult = build job: 'AndroidDevBuild', parameters: commonParams + [
+                [$class: 'StringParameterValue', name: 'buildProfile', value: buildProfile] 
+                ], propagate: false, wait: true
+                
+                
+            finalBuildNumber = "" + finalBuildResult.number
+            
+            //def finalBuildCompletePath = finalBuildResult.getBuildVariables().WORKSPACE + "/" + dailyBuildFolder
+            //dailyBuildFolder =  "DailyBuild" + finalBuildNumber
+            
+            //sh "echo ${dailyBuildFolder}"
+
+        }
+        
+        buildProfile = "googleRelease"
+            
+        stage("ReleaseBuildAndroid" + projectFolder) {
+      
+            build job: 'AndroidDevBuild', parameters: commonParams + [
+                [$class: 'StringParameterValue', name: 'buildProfile', value: buildProfile],
+                [$class: 'StringParameterValue', name: 'buildNumOverride', value: finalBuildNumber]
+                ], propagate: false, wait: true
+        }
+        
+        //rename folder
+        //sh "mv -v " + DAILY_BUILD_TEMP + " " + dailyBuildFolder
+        //Upload Release build if neccessary.
+    
+}
---
+++ Pipelines/test.groovy
@@ -0,0 +1,17 @@
+node {
+    print currentBuild.getStartTimeInMillis() 
+    print currentBuild.number
+    
+    buildProfile = "iosDebug"
+    ouputFolder = "JenkinsTest"
+    projectFolder = "JenkinsTest"
+    def finalBuildResult = build job: 'TestItems', parameters: [
+        [$class: 'StringParameterValue', name: 'projectFolder', value: projectFolder]
+        ]
+        
+    //THis lets us get the variables used in this build. Will be handy.
+    def j1EnvVariables = finalBuildResult.getBuildVariables();
+    print "${j1EnvVariables}" 
+    
+    
+}
