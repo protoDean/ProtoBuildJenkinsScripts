f72186601e1b2aab084ae59129977d29985f3e3c
dean@protostargames.com <dean@protostargames.com>
2019-11-21 10:30:56 +1000

    Dean remvoed buidl

--- Pipelines/test.groovy
+++ Pipelines/test.groovy
@@ -5,9 +5,9 @@ node {
     buildProfile = "iosDebug"
     ouputFolder = "JenkinsTest"
     projectFolder = "JenkinsTest"
-    def finalBuildResult = build job: 'TestItems', parameters: [
-        [$class: 'StringParameterValue', name: 'projectFolder', value: projectFolder]
-        ]
+    //def finalBuildResult = build job: 'TestItems', parameters: [
+    //    [$class: 'StringParameterValue', name: 'projectFolder', value: projectFolder]
+    //    ]
         
     //THis lets us get the variables used in this build. Will be handy.
     def j1EnvVariables = finalBuildResult.getBuildVariables();
