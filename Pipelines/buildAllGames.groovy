a5385d653af67c3e746cf93deddb22b12d9e7452
dean@protostargames.com <dean@protostargames.com>
2019-11-22 11:10:30 +1000

    Fixes

--- Pipelines/buildAllGames.groovy
+++ Pipelines/buildAllGames.groovy
@@ -1,13 +1,16 @@
-import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
 
 node{
 	def dailyBuild = load(pwd() + "@script/Pipelines/Common/buildGame.groovy")
 
 	File file = new File("/Volumes/StoreSafe/Jenkins/BuildSettings/dailyBuilds.json")
-	def dailyBuildSettings = new JsonSlurper().parseText(file.text);
+	def dailyBuildSettings = new JsonSlurperClassic().parseText(file.text);
 
 	for (game in dailyBuildSettings.games) {
-   		dailyBuild.DoGame(game.projectName , game.sourceBranch , game.unityVersion , game.targets);
+		if(game.buildDebug)
+   		{
+		   dailyBuild.DoGame(game.projectName , game.sourceBranch , game.unityVersion , game.targets);
+		}
 	}
 
 	//dailyBuild.DoGame("LawnMower" ,"default" , "2019.2.9f1");
