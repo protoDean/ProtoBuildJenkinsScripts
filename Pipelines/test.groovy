6735485fc42c005b8b0016e7bd45af259afa2f68
dean@protostargames.com <dean@protostargames.com>
2019-11-26 11:10:30 +1000

    Dean test blodck

--- Pipelines/test.groovy
+++ Pipelines/test.groovy
@@ -1,6 +1,8 @@
 
 import groovy.json.JsonSlurper
 
+//Using Slack
+//https://github.com/jenkinsci/slack-plugin/blob/master/README.md
 node {
     print currentBuild.getStartTimeInMillis() 
     print currentBuild.number
@@ -62,6 +64,34 @@ node {
 		]
 	]
 	slackSend( attachments: attachments)
+
+
+	blocks = [
+	[
+		"type": "section",
+		"text": [
+			"type": "mrkdwn",
+			"text": "Hello, Assistant to the Regional Manager Dwight! *Michael Scott* wants to know where you'd like to take the Paper Company investors to dinner tonight.\n\n *Please select a restaurant:*"
+		]
+	],
+    [
+		"type": "divider"
+	],
+	[
+		"type": "section",
+		"text": [
+			"type": "mrkdwn",
+			"text": "*Farmhouse Thai Cuisine*\n:star::star::star::star: 1528 reviews\n They do have some vegan options, like the roti and curry, plus they have a ton of salad stuff and noodles can be ordered without meat!! They have something for everyone here"
+		],
+		"accessory": [
+			"type": "image",
+			"image_url": "https://s3-media3.fl.yelpcdn.com/bphoto/c7ed05m9lC2EmA3Aruue7A/o.jpg",
+			"alt_text": "alt text for image"
+			]
+		]
+	]
+	slackSend( blocks: blocks)
+
 
 	//didnt work
 	//slackUploadFile(filePath : "testFile.txt" , channel : "#builds")
