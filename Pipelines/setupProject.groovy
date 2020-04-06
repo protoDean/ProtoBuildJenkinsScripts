b9e842759e178bd6e4960b6cc8cfb3da7c8f968f
Dean Loades <dean@protostargames.com>
2020-04-06 15:52:54 +1000

    Dean - SetupProject Pipeline.

--- Pipelines/setupProject.groovy
+++ Pipelines/setupProject.groovy
@@ -11,6 +11,27 @@ node{
 	// https://github.com/protoDean/${projectFolder}.git
 	sh "/usr/bin/git clone https://github.com/protoDean/${sourceProject}.git ${env.PROJECT_PATH}/${projectFolder}"
 	
-	currentBuild.description = "Cloned https://github.com/protoDean/${sourceProject}.git to ${projectFolder}"
+	
+
+
+		//git(url:"https://github.com/protoDean/${projectFolder}", branch: "${sourceBranch}" , credentialsId:"JenkinsGithubLogin")
+		
+		withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'JenkinsGithubLogin',
+		usernameVariable: 'credUser', passwordVariable: 'credPassword']]) 
+		{
+			//sh "/usr/bin/git lfs install"
+		
+			//sh "/usr/bin/git checkout -f ${sourceBranch}"
+			//sh "/usr/bin/git lfs pull https://${credUser}:${credPassword}@github.com/protoDean/${projectFolder}"
+
+			sh "/usr/bin/git remote --set-url origin https://${credUser}:${credPassword}@github.com/protoDean/${projectFolder} &&" +
+				"/usr/bin/git clone https://${credUser}:${credPassword}@github.com/protoDean/${sourceProject}.git ${env.PROJECT_PATH}/${projectFolder}"
+
+			//Set it back to the non passwork version.
+			sh "/usr/bin/git remote --set-url origin https://github.com/protoDean/${projectFolder}" 
+		}
+		
+		currentBuild.description = "Cloned https://github.com/protoDean/${sourceProject}.git to ${projectFolder}"
+	
 
 }
