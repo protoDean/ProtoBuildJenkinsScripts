463ad64a274f41170020651e446741b74d0b6627
Dean Loades <dean@protostargames.com>
2020-04-06 15:57:51 +1000

    Dean try again

--- Pipelines/setupProject.groovy
+++ Pipelines/setupProject.groovy
@@ -24,11 +24,10 @@ node{
 			//sh "/usr/bin/git checkout -f ${sourceBranch}"
 			//sh "/usr/bin/git lfs pull https://${credUser}:${credPassword}@github.com/protoDean/${projectFolder}"
 
-			sh "/usr/bin/git remote --set-url origin https://${credUser}:${credPassword}@github.com/protoDean/${projectFolder} &&" +
-				"/usr/bin/git clone https://${credUser}:${credPassword}@github.com/protoDean/${sourceProject}.git ${env.PROJECT_PATH}/${projectFolder}"
+			sh "/usr/bin/git clone https://${credUser}:${credPassword}@github.com/protoDean/${sourceProject}.git ${env.PROJECT_PATH}/${projectFolder}"
 
 			//Set it back to the non passwork version.
-			sh "/usr/bin/git remote --set-url origin https://github.com/protoDean/${projectFolder}" 
+			sh "/usr/bin/git remote --set-url origin https://github.com/protoDean/${sourceProject}" 
 		}
 		
 		currentBuild.description = "Cloned https://github.com/protoDean/${sourceProject}.git to ${projectFolder}"
