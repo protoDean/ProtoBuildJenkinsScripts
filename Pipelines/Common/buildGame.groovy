5c44b2027979b2950287b16dc54589024fc78101
Dean Loades <dean@protostargames.com>
2020-11-26 11:22:32 +1000

    Dean - try a lfs checkout

--- Pipelines/Common/buildGame.groovy
+++ Pipelines/Common/buildGame.groovy
@@ -92,6 +92,7 @@ def DoGamePlatform(game , boolean alwaysBuild , gameResult , dailyBuildFolder ) 
 
 			runShell("git fetch --tags --force https://${JENKINS_GITHUB_USER}@github.com/protoDean/${projectFolder} +refs/heads/*:refs/remotes/origin/*")
 			runShell("git checkout -f -B ${sourceBranch} origin/${sourceBranch}")
+			runShell("git lfs checkout")
 			runShell("git submodule update --init --recursive")
 			runShell("git submodule foreach git lfs pull")
 			
